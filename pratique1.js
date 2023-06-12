936cc7a220dfc64641aa18ff05a7b5c889b4794f
Bastien <bastfv@gmail.com>
2023-06-12 22:39:47 +0200

    log

--- pratique1.js
+++ pratique1.js
@@ -1,8 +1,11 @@
 /* Function calculant la moyenne de 3 paramètres */
 const average = new Function("a", "b", "c", "return (a + b + c) / 3");
+console.log("Moyenne:", average(10, 15, 8));
 
 /* Tableau contenant 4 éléments */
-const array = new Array("RS 6", 100000, 700, 10);
+const array = new Array();
+array.push(1, 2, 3, 4);
+console.log(array);
 
 /* Objet contenant 2 propriétés et 1 methode */
 const object = new Object();
@@ -11,12 +14,9 @@ object.lastname = "Doe";
 object.sayHello = function() {
     return "Bonjour";
 };
+console.log(object);
+console.log(object.sayHello());
 
 /* Erreur */
 const error = new Error("What's going on ? And I say heyyyyyy");
-
-console.log("Moyenne:", average(10, 15, 8));
-console.log(array);
-console.log(object);
-console.log(object.sayHello());
 throw error;
