0ea8b4d94db0cbf0c94722300eb669f8f6c31281
Bastien <bastfv@gmail.com>
2023-06-14 08:09:50 +0200

    practice

--- pratique3.js
+++ pratique3.js
@@ -1,9 +1,13 @@
-// Création de l'objet Products
-function Products(name, purchasePrice, sellingPrice, description) {
-    this.name = name,
-    this.purchasePrice = purchasePrice,
-    this.sellingPrice = sellingPrice,
-    this.description = description
+const crypto = require('crypto');
+
+/**
+ * Fonction constructeur Product
+ */
+function Product(name, purchasePrice, sellingPrice, description) {
+    this.name = String(name).trim();
+    this.purchasePrice = Number(purchasePrice);
+    this.sellingPrice = Number(sellingPrice);
+    this.description = String(description).trim();
 
     Object.defineProperties(this, {
         id: {
@@ -19,46 +23,45 @@ function Products(name, purchasePrice, sellingPrice, description) {
             }
         },
         ref: {
-            value: this.name + "-" + Date.now()
+            value: this.name.toLocaleUpperCase() + "-" + Date.now()
         }
     });
 }
 
-const playstation = new Products("ps5", 500, 600, "console");
+const product1 = new Product("ps5", 200, 300, "console");
+console.log(product1);
 
-
-// Création d'un proxy 
-const playstationProxy = new Proxy(playstation, {
+/**
+ * Proxy pour l'objet Product
+ */
+const productProxy = new Proxy(product1, {
     get(obj, prop) {
-        if (prop === "purchasePrice" || prop === "sellingPrice") {
-            return obj[prop].toFixed(2) + "€";
-        };
+        if (prop === "purchasePrice" | prop === "sellingPrice") {
+            return obj[prop].toFixed(2) + " €";
+        }
 
-        if (prop === "description" && obj[prop].length >= 100) {
+        if (prop === "description" && obj[prop].length > 100) {
             return obj[prop].slice(0, 99) + "...";
-        };
+        }
+        return obj[prop];
     },
     set(obj, prop, value) {
-        if (prop === "purchasePrice" || prop === "sellingPrice") {
+        if (prop === "puchasePrice" | prop === "sellingPrice") {
             if (value < 0) {
-                throw new Error("Must be a positive number");
+                throw new Error(`${prop} must be a positive number`);
             }
-        };
-
-        if (prop === "sellingPrice" && value < obj.purchasePrice) {
-            throw new Error(`sellingPrice must be greater than or equal to ${obj.purchasePrice.toFixed(2)} €`);
-        };
-
-        if (prop === "purchasePrice" && value > obj.sellingPrice) {
-            throw new Error(`purchasePrice must be less or equal to ${obj.sellingPrice.toFixed(2)} €`);
         }
+
+        if (prop === "sellingPrice" && obj.purchasePrice > value) {
+            throw new Error(`sellingPrice must be at least ${obj.purchasePrice}`);
+        }
+
+        if (prop === "purchasePrice" && obj.sellingPrice < value) {
+            throw new Error(`purchasePrice must be at least ${obj.sellingPrice}`);
+        }
+
+        return obj[prop].value;
     }
 });
 
-console.log(playstation);
-console.log(playstation.margin);
-console.log(playstation.ref);
-console.log("-------------");
-playstationProxy.sellingPrice = 200;
-console.log(playstationProxy.purchasePrice);
-console.log(playstationProxy.sellingPrice);
+console.log(productProxy.sellingPrice);
